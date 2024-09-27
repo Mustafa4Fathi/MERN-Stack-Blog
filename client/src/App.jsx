a9717b0f4c328d0be251410029efef60a134b699
Mustafa Fathi <abmostafa0335@gamil.com>
2024-09-27 22:17:21 +0300

    Create the Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,25 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import Blogs from "./pages/Blogs";
 
 const App = () => {
+
   return (
-    <BrowserRouter>
+    <div>
+      <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 };
 
